34b2af01c437551de756e111a3d5a61d4895fc68
JJaeggm <raptor1130@gmail.com>
2024-01-23 17:07:51 +0900

    20240123

---
+++ 04/js/callback.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+  alert("클릭했습니다.")
+})
---
+++ 05/js/classlist-3.js
@@ -0,0 +1,11 @@
+// const title = document.querySelector("#title");
+
+// title.onclick = () => {
+//   title.classList.toggle("clicked");
+// }
+
+const title = document.querySelector('#title');
+
+title.onclick = () => {
+  title.classList.toggle('clicked');
+};
